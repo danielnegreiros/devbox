4673227351a17e9b71d7c28604fd36bd3b799b00
Daniel Barros <59064132+danielnegreiros@users.noreply.github.com>
2024-10-04 16:00:47 -0300

    ubuntu 22 and 24

--- internal/domain/usecases/conf/cloud_init_conf.go
+++ internal/domain/usecases/conf/cloud_init_conf.go
@@ -6,7 +6,8 @@ import (
 
 func GetImageUrl(imageName string) (string, error) {
 	images_url := map[string]string{
-		"ubuntu-latest": "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img",
+		"ubuntu-22": "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img",
+		"ubuntu-24": "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img",
 		"fedora-38":     "https://download.fedoraproject.org/pub/fedora/linux/releases/38/Cloud/x86_64/images/Fedora-Cloud-Base-38-1.6.x86_64.qcow2",
 	}
 
